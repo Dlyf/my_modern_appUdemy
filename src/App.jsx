ad959bdaeddf367eb0a7ed6dc83b11de37ac4772
Dlyf <domilung@gmail.com>
2023-02-12 18:08:15 +0100

    fin css + bulma + rsuitejs

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,54 @@
-import ProfileCard from './ProfileCard'
+import "bulma/css/bulma.css";
+import "rsuite/dist/rsuite.css";
+import { Panel, Placeholder, Row, Col } from "rsuite";
+import ProfileCard from "./ProfileCard";
+import AlexaImage from "./images/alexa.png";
+import CortanaImage from "./images/cortana.png";
+import SiriImage from "./images/siri.png";
 
+console.log(AlexaImage);
+console.log(SiriImage);
 function App() {
-    return(
-        <div>
-            <div>Personal Digital Assistants</div>
-            {/* Adding props */}
-            <ProfileCard title="Alexa" handle="@alexa99"/>
-            <ProfileCard title="Cortana" handle="@cortana32"/>
-            <ProfileCard title="Siri" handle="@siri01"/>
+  return (
+    <div>
+      <section class="hero is-primary">
+        <div class="hero-body">
+          <p class="title">Personal Digital Assistants</p>
         </div>
-    );
+      </section>
+      <div className="container">
+        <Row>
+          <Col md={5}>
+            <ProfileCard
+              title="Alexa" 
+              handle="@alexa99" 
+              image={AlexaImage}
+              description="Cortana was made by Microsoft. Who knows what it odes?" />
+          </Col>
+          <Col md={5}>
+            <ProfileCard
+              title="Cortana"
+              handle="@cortana32"
+              image={CortanaImage}
+              description="Alexa was created by Amazon and helps you buy things."
+            />
+          </Col>
+          <Col md={5}>
+            <ProfileCard
+              title="Siri" 
+              handle="@siri01" 
+              image={SiriImage} 
+              description="Siri was made by Apple and is being phased out."
+            />
+          </Col>
+        </Row>
+      </div>
+
+      {/* <img src={AlexaImage} />
+            <img src={SiriImage} /> */}
+      {/* Adding props */}
+    </div>
+  );
 }
 
 export default App;
--- src/ProfileCard.jsx
+++ src/ProfileCard.jsx
@@ -1,17 +1,31 @@
+import { Panel } from "rsuite";
 // receive this object props
-// 3ème façon 
-function ProfileCard({ title, handle }) {
-    // Argument destructuring
-    // const title = props.title;
-    // const handle = props.handle;
-    //2ème façon
-    // const { title, handle } = props;
-    return(
-        <div>
-            <div>Title is {title}</div>
-            <div>Handle is {handle}</div>
-        </div>
-    )
+// 3ème façon
+function ProfileCard({ title, handle, image, description }) {
+  // Argument destructuring
+  // const title = props.title;
+  // const handle = props.handle;
+  //2ème façon
+  // const { title, handle } = props;
+  return (
+    <Panel
+      shaded
+      bordered
+      bodyFill
+      style={{ display: "inline-block", width: 240, marginLeft:"20rem" }}
+    >
+      <img src={image} alt="pda logo" height="240"/>
+      <Panel header={<h5>{title}</h5>}>
+        <p>{handle}</p>
+        <div className="content">{description}</div>
+      </Panel>
+    </Panel>
+    // <div className="card">
+    //     <img src={image} alt="pda logo"/>
+    //     <div>Title is {title}</div>
+    //     <div>Handle is {handle}</div>
+    // </div>
+  );
 }
 
 export default ProfileCard;
